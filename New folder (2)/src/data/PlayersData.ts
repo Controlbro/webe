3ef34941827d280bd5f05e36428474c79634cc5c
Controlbro <54811794+Controlbro@users.noreply.github.com>
2025-12-18 23:26:54 -0500

    Update PlayersData.ts

--- New folder (2)/src/data/PlayersData.ts
+++ New folder (2)/src/data/PlayersData.ts
@@ -3,10 +3,10 @@ export interface PlayerProfile {
 }
 
 export const playersData: PlayerProfile[] = [
-  { name: 'Steve' },
-  { name: 'Alex' },
-  { name: 'MumboJumbo' },
-  { name: 'Agnes' },
-  { name: 'Jeb_' },
-  { name: 'Kai' },
+  { name: '420MegaBaller' },
+  { name: 'CBYT' },
+  { name: 'G1assDa1e' },
+  { name: 'RainWasHere' },
+  { name: 'SeaEquation1111' },
+  { name: 'FaithAndOnly' },
 ];
